e8fa2c34fa9b6224a2dfcadfc6b118064872577a
ryoo14 <anana12185@gmail.com>
2024-02-11 00:04:17 +0900

    refactor: separate modules(#7)

--- main.ts
+++ main.ts
@@ -1,113 +1,7 @@
 import { parseFeed } from "rss"
-import { TextToSpeechClient } from "tts"
-
-function debug(entries: Entry[]) {
-  const minLength = entries.sort((a, b) => a.text.length - b.text.length)[0].title.length
-  for (const e of entries) {
-    console.log(`${e.title.slice(0, minLength)}: ${e.text.length}`)
-  }
-  console.log(entries.reduce((s, e) => s + e.text.length, 0))
-}
-
-async function silverHarp(title: string, url: string): Promise<Entry> {
-  const entry = await fetchEntry(title, url)
-  const entryWithAudio = await textToSpeech(entry)
-  return entryWithAudio
-}
-
-async function fetchEntry(title: string, url: string): Promise<Entry> {
-  try {
-    const res = await fetch(url)
-    const text = await res.text()
-    return {
-      title: title,
-      url: url,
-      text: removeHTMLTags(text),
-      audio: null,
-    }
-  } catch {
-    console.error(`Faied to fetch data from ${title}.`)
-    return {
-      title: title,
-      url: url,
-      text: "",
-      audio: null,
-    }
-  }
-}
-
-function removeHTMLTags(text: string): string {
-  return text.replace(/(<(head|script|style|header|footer|nav|iframe|aside)[^>]*>([\s\S]*?)<\/(head|script|style|header|footer|nav|iframe|aside)>)/g, "")
-    .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")
-    .replace(/\s{2,}/g, " ")
-    .replace(/anond:[0-9]{14}/g, "。") // hatena masuda
-    .replace(/≪前の記事[\s\S]*/, "").replace(/≫次の記事[\s\S]*/, "") // publickey
-}
-
-async function textToSpeech(entry: Entry): Promise<Entry> {
-  try {
-    if (entry.text.length === 0 || entry.text.length > 5000) {
-      throw new Error(`${entry.title} has 0 or larger than 5000 text.`)
-    }
-
-    const audioArray = await generateAudio(entry.text)
-    entry.audio = combineAudio(audioArray)
-
-    return entry
-  } catch (e) {
-    console.error(e.message)
-    entry.audio = null
-    return entry
-  }
-}
-
-async function generateAudio(text: string) {
-  const client = new TextToSpeechClient()
-  const audioArray = []
-  for (let i = 0; i < text.length; i += 1500) {
-    const request = {
-      input: { text: text.slice(i, i + 1500) },
-      voice: {
-        languageCode: "ja-JP",
-        ssmlGender: "FEMALE",
-        name: "ja-JP-Wavenet-A", // "ja-JP-Wavenet-A", "ja-JP-Standard-A", "ja-JP-Neural2-B"
-      },
-      audioConfig: {
-        audioEncoding: "MP3",
-        speakingRate: 1.5,
-      },
-    }
-    const [response] = await client.synthesizeSpeech(request)
-    audioArray.push(response.audioContent)
-  }
-  return audioArray
-}
-
-function combineAudio(audioArray) {
-  const combinedLength = audioArray.reduce((p, c) => p + c.length, 0)
-  const combined = new Uint8Array(combinedLength)
-  let position = 0
-  for (const a of audioArray) {
-    combined.set(a, position)
-    position += a.length
-  }
-  return combined
-}
-
-function shouldSkipUrl(url: string): boolean {
-  const skipPatterns = [
-    'www\.itmedia\.co\.jp',
-    'togetter\.com',
-  ];
-  return skipPatterns.some(pattern => url.match(new RegExp(pattern)));
-}
-
-type Entry = {
-  title: string
-  url: string
-  text: string
-  audio: Uint8Array | null
-}
+import { shouldSkipUrl } from "./utils.ts"
+import { silverHarp } from "./textToSpeech.ts"
+import { Entry } from "./types.ts"
 
 // Fetch RSS Feed and parse
 const res = await fetch("https://b.hatena.ne.jp/hotentry/it.rss")
@@ -130,6 +24,14 @@ for (const e of entries) {
     continue
   }
   Deno.writeFileSync(`${Deno.cwd()}/mp3/${e.title.replace(/\//g, "-")}.mp3`, e.audio)
+}
+
+function debug(entries: Entry[]) {
+  const minLength = entries.sort((a, b) => a.text.length - b.text.length)[0].title.length
+  for (const e of entries) {
+    console.log(`${e.title.slice(0, minLength)}: ${e.text.length}`)
+  }
+  console.log(entries.reduce((s, e) => s + e.text.length, 0))
 }
 
 //debug(entries)
---
+++ types.ts
@@ -0,0 +1,6 @@
+export type Entry = {
+  title: string
+  url: string
+  text: string
+  audio: Uint8Array | null
+}
