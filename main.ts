0a9519d7599169e75dd3be92f39db94acd3f2829
ryoo14 <anana12185@gmail.com>
2024-03-06 23:35:28 +0900

    refactor: delete unnecessary processes and appropriately added/updated comments

--- main.ts
+++ main.ts
@@ -3,9 +3,10 @@ import { silverHarp } from "./textToSpeech.ts"
 import { Entry } from "./types.ts"
 import { getTextAndDeleteBookmarks } from "./instapaper.ts"
 
+// Get articles and their texts from Instapaper bookmarks, then delete the bookmarks
 const entriesWithNullAudio: Entry[] = await getTextAndDeleteBookmarks()
 
-// Fetch and Audionaize contents per feeds
+// Text to speech
 const promiseEntries: Promise<Entry>[] = []
 for (const e of entriesWithNullAudio) {
   promiseEntries.push(silverHarp(e))
@@ -14,23 +15,11 @@ for (const e of entriesWithNullAudio) {
 const entries: Entry[] = await Promise.all(promiseEntries)
 const entriesExcludeNullAudio: Entry[] = entries.filter((e) => e.audio != null)
 
-// Merge audio
-//const audioArray: Uint8Array[] = entriesExcludeNullAudio.map((e) => e.audio as Uint8Array)
-//const audio: Uint8Array = combineAudio(audioArray)
-//const yyyymmddhh: string = getCurrentTimestamp()
-//Deno.writeFileSync(`${Deno.cwd()}/mp3/${yyyymmddhh}.mp3`, audio as Uint8Array)
-
+// Output the article's title and its length, and then save the audio file
 for (const e of entriesExcludeNullAudio) {
-  // Create MP3
   const length = e.text.length
   const title = e.title
-  let count = 0
-  for (let i = 0; i < e.text.length; i++) {
-    if (e.text[i] === "。") {
-      count++
-    }
-  }
-  console.log(`${title}: ${length}(${count})`)
+  console.log(`${title}: ${length}`)
   Deno.writeFileSync(`${Deno.cwd()}/mp3/${e.title.replace(/[\|\/]/g, "-")}.mp3`, e.audio as Uint8Array)
 }
 
