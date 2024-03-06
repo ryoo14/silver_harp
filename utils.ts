effeeb94b48ede51984167e27ce226b53420490e
ryoo14 <anana12185@gmail.com>
2024-03-06 23:44:13 +0900

    refactor: delete unused function shouldSkipUrl
    - #17

--- utils.ts
+++ utils.ts
@@ -8,15 +8,6 @@ export function removeHTMLTags(text: string): string {
     .replace(/≪前の記事[\s\S]*/, "").replace(/≫次の記事[\s\S]*/, "") // publickey
 }
 
-export function shouldSkipUrl(url: string): boolean {
-  const skipPatterns = [
-    "www\.itmedia\.co\.jp",
-    "togetter\.com",
-    "speakerdeck\.com",
-  ]
-  return skipPatterns.some((pattern) => url.match(new RegExp(pattern)))
-}
-
 export function checkResponseCode(response: Response): boolean {
   let flag
   if (response.status === 200) {
